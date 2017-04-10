7efa14b863e8e73008cba69fc09786a3b4cfc9b1
Brienyll <brienyll29@gmail.com>
2017-04-10 15:37:18 -0700

    Prints String to Char
    used for loop to iterate string

---
+++ arrayPrintChar.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+using namespace std;
+
+int main( int argc, char ** argv) {
+  char s[] = { 's', 't', 'r', 'i', 'n', 'g'};
+  for (int i = 0; s[i];i++) {
+    printf ("char  is: %c\n", s[i]);
+  }
+  return 0;
+}
