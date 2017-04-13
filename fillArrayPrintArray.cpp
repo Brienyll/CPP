a8f4040b1c0103472839f5ccb4b0d7e500e15c19
Brienyll <brienyll29@gmail.com>
2017-04-12 21:12:44 -0700

    Create fillArrayPrintArray.cpp

---
+++ fillArrayPrintArray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void fillArray(int n[], int size);
+void printArray(int n[], int size);
+
+int main() {
+	int num[10];
+	int size;
+	fillArray(num, 10);
+	printArray(num, 10);
+	system("pause");
+}
+
+void fillArray(int n[], int size) {
+	for (int i = 0; i < size; i++) {
+		n[i] = rand()%100;
+	}
+}
+
+void printArray(int n[], int size) {
+	for (int i = 0; i < size; i++) {
+	cout << n[i] << endl;
+	}
+}
