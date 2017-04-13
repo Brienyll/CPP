7bc408663a99d2cce6a51375b408e2a7612d8544
Brienyll <brienyll29@gmail.com>
2017-04-12 20:25:03 -0700

    Create functionsAddNumbers.cpp
    used prototypes

---
+++ functionsAddNumbers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+//prototypes
+void printHello();
+void getNums(int &x, int &y);
+void addNums(int x, int y, int &z);
+void printRes(int z);
+
+int main() {
+	int x, y, z;
+	printHello();
+	getNums(x, y);
+	addNums(x, y, z);
+	printRes(z);
+	system("pause");
+}
+//functions
+void printHello() {
+	cout << "Hello" << endl;
+}
+
+void getNums(int &n1, int &n2) {
+	cin >> n1 >> n2;
+}
+
+void addNums(int n1, int n2, int &n3) {
+	n3 = n1 + n2;
+}
+
+void printRes(int n1) {
+	printf("Sum is = %i\n", n1);
+}
