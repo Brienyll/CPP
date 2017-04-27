6f69f99b77b324deb242b181cab7123d4ad5b20b
Brienyll <brienyll29@gmail.com>
2017-04-26 21:57:21 -0700

    Create readFileData.cpp

---
+++ readFileData.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+void fillArray(int n[], int size);
+void printArray(int n[], int size);
+//void findNumber(int n[], int size, int num);
+void readData(int n[]);
+void findCount(int n[], int size, int num);
+
+int main() {
+	int num[1000];
+	fillArray(num, 1000);
+	printArray(num, 1000);
+	//	findNumber(num, 1000, 19);
+	readData(num);
+	findCount(num, 1000, 19);
+	system("pause");
+}
+
+void fillArray(int n[], int size) {
+	for (int i = 0; i < size; i++) {
+		n[i] = rand() % 100;
+	}
+}
+
+void printArray(int n[], int size) {
+	ofstream outData;
+	outData.open("C:\\temp\\test.txt", ios::app);
+	for (int i = 0; i < size; i++) {
+		outData << n[i] << endl;
+	}
+	outData.close();
+}
+
+//void findNumber(int n[], int size, int num) {
+//	ifstream inData;
+//	inData.open("C:\\temp\\test.txt");
+//	int count = 0;
+//	for (int i = 0; i < size; i++) {
+//		if (n[i] == num) {
+//			count += 1;
+//		}
+//	}
+//	inData.close();
+//}
+
+void findCount(int n[], int size, int num) {
+	ofstream outData;
+	int count = 0;
+	for (int i = 0; i < size; i++) {
+		if (n[i] == num) {
+			count += 1;
+		}
+	}
+
+	outData.open("C:\\temp\\test.txt", ios::app);
+	outData << "count is: " << count << endl;
+	outData.close();
+}
+void readData(int n[]) {
+	ifstream inData;
+	int i;
+
+	inData.open("C:\\temp\\test.txt");
+	while (!inData.eof()) {
+
+		inData >> i;
+		inData.close();
+	}
+}
